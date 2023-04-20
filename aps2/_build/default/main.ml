f8e8fe4754de90e9f12dc6b6d4f4f7a041f9e64f
shuhan-duan <shuhanduan99@gmail.com>
2023-04-20 11:06:18 +0200

    aps2

---
+++ aps2/_build/default/main.ml
@@ -0,0 +1,37 @@
+open Lexing
+
+(*For error messages*)
+let err msg pos =
+  Printf.eprintf
+    "Error on line %d col %d: %s.\n"
+    pos.pos_lnum
+    (pos.pos_cnum - pos.pos_bol)
+    msg;
+  exit 1
+;;
+
+print_endline ""
+
+let () =
+  if Array.length Sys.argv != 2
+  then (
+    Printf.eprintf "Usage: %s <file>\n" Sys.argv.(0);
+    exit 1);
+
+  (*Open the file to compile*)
+  let f = open_in Sys.argv.(1) in
+
+  (* Tokenize the file *)
+  let buf = Lexing.from_channel f in
+
+  try
+    let output = Parser.prog Lexer.token buf in
+      close_in f;
+    Evaluateur.eval_prog output;
+    (* let res = Printer.string_of_prog output in
+    print_string res;
+    print_string ".\n"; *)
+  with
+    | Lexer.Eof ->
+      exit 0;
+;;
